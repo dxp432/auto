d70a0321793f99f3db12540defcb132d23c5e8ca
小鹏 <dxp432@163.com>
2020-10-21 10:19:36 +0800

    Update auto.py
    模拟电脑点击右键

--- auto.py
+++ auto.py
@@ -55,6 +55,20 @@ def computer_click(x, y):
     ctypes.windll.user32.SetCursorPos(x, y)
     ctypes.windll.user32.mouse_event(2, 0, 0, 0, 0)
     ctypes.windll.user32.mouse_event(4, 0, 0, 0, 0)
+    '''
+    MOUSE_LEFTDOWN = 0x0002     # left button down 
+    MOUSE_LEFTUP = 0x0004       # left button up 
+    MOUSE_RIGHTDOWN = 0x0008    # right button down 
+    MOUSE_RIGHTUP = 0x0010      # right button up 
+    MOUSE_MIDDLEDOWN = 0x0020   # middle button down 
+    MOUSE_MIDDLEUP = 0x0040     # middle button up 
+    '''
+
+def computer_right_click(x, y):
+    ''' 模拟电脑点击右键 '''
+    ctypes.windll.user32.SetCursorPos(x, y)
+    ctypes.windll.user32.mouse_event(8, 0, 0, 0, 0)
+    ctypes.windll.user32.mouse_event(10, 0, 0, 0, 0)
 
 
 # 双击
